d84d477157e47a33aa1f74d96d7d0f30297cb73a
ben <ben@example.com>
2023-01-08 17:12:32 +0800

    part7 routed-anecdotes updated v1

--- part7/routed-anecdotes/src/App.js
+++ part7/routed-anecdotes/src/App.js
@@ -138,23 +138,22 @@ const App = () => {
 
   const addNew = (anecdote) => {
     anecdote.id = Math.round(Math.random() * 10000)
-    console.log(anecdote);
     setAnecdotes([...anecdotes, anecdote])
   }
 
-  const anecdoteById = (id) =>
-    anecdotes.find(a => a.id === id)
-
-  const vote = (id) => {
-    const anecdote = anecdoteById(id)
-
-    const voted = {
-      ...anecdote,
-      votes: anecdote.votes + 1
-    }
-
-    setAnecdotes(anecdotes.map(a => a.id === id ? voted : a))
-  }
+  /*  const anecdoteById = (id) =>
+     anecdotes.find(a => a.id === id)
+ 
+   const vote = (id) => {
+     const anecdote = anecdoteById(id)
+ 
+     const voted = {
+       ...anecdote,
+       votes: anecdote.votes + 1
+     }
+ 
+     setAnecdotes(anecdotes.map(a => a.id === id ? voted : a))
+   } */
 
   return (
     <div>
